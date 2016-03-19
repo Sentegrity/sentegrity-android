9f37bce6a62a0ae58c1ebfcba7a52747ad7508af
Davor Mestrovic <davor@profico.hr>
2016-03-20 00:08:57 +0100

    initial implementation of the login screen and detection fail dialog

---
+++ sentegrity/src/main/java/com/sentegrity/android/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.sentegrity.android;
+
+import android.app.Dialog;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showError();
+            }
+        }, 1000);
+    }
+
+    private void showError() {
+        Dialog dialog = new Dialog(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_login_error, null);
+        dialog.setContentView(dialogView);
+
+        dialog.show();
+    }
+}
