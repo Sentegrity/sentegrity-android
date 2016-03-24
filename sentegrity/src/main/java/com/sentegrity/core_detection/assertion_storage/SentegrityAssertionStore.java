19b4e4783d348b21e44986c54835fcc45fe9a3d7
Davor Mestrovic <davor@profico.hr>
2016-03-24 21:01:19 +0100

    trustfactor/assertion store file read/write

--- sentegrity/src/main/java/com/sentegrity/core_detection/assertion_storage/SentegrityAssertionStore.java
+++ sentegrity/src/main/java/com/sentegrity/core_detection/assertion_storage/SentegrityAssertionStore.java
@@ -2,10 +2,37 @@ package com.sentegrity.core_detection.assertion_storage;
 
 import android.content.Context;
 
+import com.google.gson.annotations.SerializedName;
+import com.sentegrity.core_detection.policy.SentegrityTrustFactor;
+
+import java.util.List;
+
 /**
  * Created by dmestrov on 22/03/16.
  */
 public class SentegrityAssertionStore {
+
+    @SerializedName("appid")
+    private String appId;
+
+    @SerializedName("storedTrustfactorObject")
+    private List<SentegrityTrustFactor> trustFactors;
+
+    public List<SentegrityTrustFactor> getTrustFactors() {
+        return trustFactors;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public void setTrustFactors(List<SentegrityTrustFactor> trustFactors) {
+        this.trustFactors = trustFactors;
+    }
 
     private static SentegrityAssertionStore sInstance;
 
--- sentegrity/src/main/java/com/sentegrity/core_detection/assertion_storage/SentegrityTrustFactorStore.java
+++ sentegrity/src/main/java/com/sentegrity/core_detection/assertion_storage/SentegrityTrustFactorStore.java
@@ -2,6 +2,13 @@ package com.sentegrity.core_detection.assertion_storage;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by dmestrov on 22/03/16.
  */
@@ -22,10 +29,51 @@ public class SentegrityTrustFactorStore {
     }
 
     public static SentegrityTrustFactorStore getInstance(){
-        if(sInstance == null || sInstance.context == null) {
+        if(sInstance == null) {
             throw new IllegalStateException("Please call CoreDetection.initialize({context}) before requesting the instance.");
         } else {
             return sInstance;
         }
+    }
+
+    public SentegrityAssertionStore setAssertionStore(SentegrityAssertionStore store, String appId){
+        //TODO: handle errors
+        String stringJson = new Gson().toJson(store);
+
+        File f = new File(storePath + "/" + appId + ".store");
+
+        try {
+            if (!f.exists()) {
+                boolean newFile = f.createNewFile();
+            }
+            FileUtils.write(f, stringJson);
+            return store;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public SentegrityAssertionStore getAssertionStore(String appId){
+        //TODO: handle errors
+        File f = new File(storePath + "/" + appId + ".store");
+
+        if (!f.exists()) {
+            return null;
+        }
+
+        SentegrityAssertionStore assertionStore;
+        String assertionJson;
+        try {
+            assertionJson = FileUtils.readFileToString(f);
+            assertionStore = new Gson().fromJson(assertionJson, SentegrityAssertionStore.class);
+            if(assertionStore != null && appId.equals(assertionStore.getAppId()))
+                return assertionStore;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
