33668529ade920cf69505e448e252aaa3d1a7802
Davor Mestrovic <davor@profico.hr>
2016-03-27 19:42:17 +0200

    added reload handler

--- sentegrity/src/main/java/com/sentegrity/android/activities/DashboardActivity.java
+++ sentegrity/src/main/java/com/sentegrity/android/activities/DashboardActivity.java
@@ -1,7 +1,9 @@
 package com.sentegrity.android.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,8 +11,11 @@ import android.widget.TextView;
 import com.sentegrity.android.R;
 import com.sentegrity.android.widget.ScoreLayout;
 import com.sentegrity.core_detection.CoreDetection;
+import com.sentegrity.core_detection.assertion_storage.SentegrityTrustFactorStore;
 import com.sentegrity.core_detection.computation.SentegrityTrustScoreComputation;
+import com.sentegrity.core_detection.dispatch.trust_factors.SentegrityTrustFactorDatasets;
 
+import java.io.File;
 import java.util.Random;
 
 
@@ -35,6 +40,7 @@ public class DashboardActivity extends MenuActivity implements View.OnClickListe
 
         findViewById(R.id.device_info).setOnClickListener(this);
         findViewById(R.id.user_info).setOnClickListener(this);
+        findViewById(R.id.reload).setOnClickListener(this);
 
         TextView deviceInfoText = (TextView) findViewById(R.id.device_info_text);
         TextView userInfoText = (TextView) findViewById(R.id.user_info_text);
@@ -66,6 +72,28 @@ public class DashboardActivity extends MenuActivity implements View.OnClickListe
                 Intent userInfo = new Intent(this, UserInfoActivity.class);
                 startActivity(userInfo);
                 break;
+            case R.id.reload:
+                showReloadAlert();
+                break;
         }
+    }
+
+    private void showReloadAlert(){
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        dialogBuilder.setTitle("Refresh");
+        dialogBuilder.setMessage("Do you want to run detection again and update the score?");
+        dialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                CoreDetection.getInstance().reset();
+                startActivity(new Intent(DashboardActivity.this, LoginActivity.class));
+                finishAffinity();
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", null);
+        AlertDialog b = dialogBuilder.create();
+        b.show();
     }
 }
--- sentegrity/src/main/java/com/sentegrity/android/activities/MenuActivity.java
+++ sentegrity/src/main/java/com/sentegrity/android/activities/MenuActivity.java
@@ -2,7 +2,6 @@ package com.sentegrity.android.activities;
 
 import android.app.Activity;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.view.Gravity;
