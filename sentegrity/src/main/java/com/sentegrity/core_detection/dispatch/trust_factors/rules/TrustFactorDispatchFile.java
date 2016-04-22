0d6881365d0ed89fca4f16422e6b0da27a50c793
Davor Mestrovic <davor@profico.hr>
2016-04-22 23:07:23 +0200

    [blacklisted] added rule - check if blacklisted file exists on the device

--- sentegrity/src/main/java/com/sentegrity/core_detection/dispatch/trust_factors/rules/TrustFactorDispatchFile.java
+++ sentegrity/src/main/java/com/sentegrity/core_detection/dispatch/trust_factors/rules/TrustFactorDispatchFile.java
@@ -1,7 +1,11 @@
 package com.sentegrity.core_detection.dispatch.trust_factors.rules;
 
 import com.sentegrity.core_detection.assertion_storage.SentegrityTrustFactorOutput;
+import com.sentegrity.core_detection.constants.DNEStatusCode;
+import com.sentegrity.core_detection.dispatch.trust_factors.SentegrityTrustFactorDatasets;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +14,28 @@ import java.util.List;
 public class TrustFactorDispatchFile {
 
     public static SentegrityTrustFactorOutput blacklisted(List<Object> payload){
-        return new SentegrityTrustFactorOutput();
+        SentegrityTrustFactorOutput output = new SentegrityTrustFactorOutput();
+
+        if (!SentegrityTrustFactorDatasets.validatePayload(payload)) {
+            output.setStatusCode(DNEStatusCode.ERROR);
+            return output;
+        }
+
+        List<String> outputList = new ArrayList<>();
+
+        for(Object objectPath : payload){
+            String path = (String) objectPath;
+
+            File f = new File(path);
+            if(!f.exists())
+                continue;
+
+            outputList.add(path);
+        }
+
+        output.setOutput(outputList);
+
+        return output;
     }
 
     @Deprecated
